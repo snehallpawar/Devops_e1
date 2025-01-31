dc6903fa1670c41f443d86383ee720b21a0373a8
snehallpawar <100402255+snehallpawar@users.noreply.github.com>
2025-02-01 00:12:05 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,84 @@
+terraform {
+  backend "s3" {
+    bucket = "467.devops.candidate.exam"
+    key    = "Santosh.Dongare"
+    region = "ap-south-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Data Sources for Existing Resources
+data "aws_nat_gateway" "nat" {
+  id = "nat-0a34a8efd5e420945"
+}
+
+data "aws_vpc" "vpc" {
+  id = "vpc-06b326e20d7db55f9"
+}
+
+data "aws_iam_role" "lambda" {
+  name = "DevOps-Candidate-Lambda-Role"
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = data.aws_vpc.vpc.id
+  cidr_block        = "10.0.1.0/24"  # Change if needed
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# Routing Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = data.aws_vpc.vpc.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# Security Group for Lambda
+resource "aws_security_group" "lambda_sg" {
+  vpc_id = data.aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lambda-security-group"
+  }
+}
+
+# Lambda Function
+resource "aws_lambda_function" "lambda_function" {
+  function_name = "devops-exam-lambda"
+  role          = data.aws_iam_role.lambda.arn
+  handler       = "index.handler"
+  runtime       = "python3.9"
+  filename      = "lambda_function.zip"
+  timeout       = 10
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_subnet.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
+
+  tags = {
+    Name = "devops-lambda"
+  }
+}
